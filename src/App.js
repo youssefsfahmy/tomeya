0268e50460f0c51747d4570f620af2bb25d52fb2
unknown <Nadimramy@gmail.com>
2021-08-25 16:04:23 +0200

    structure and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/SignUp";
+import ToDoList from "./Pages/ToDoList";
+import Notes from "./Pages/Notes";
+import Home from "./Pages/Home";
+import NavBar from "./Components/General/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route path="/home" component={Home} exact />
+        <Route path="/" component={Login} exact />
+        <Route path="/signup" component={SignUp} exact />
+        <Route path="/todolist" component={ToDoList} exact />
+        <Route path="/notes" component={Notes} exact />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/SignUp/SignUpCard.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SignUpCard() {
+  return <div></div>;
+}
